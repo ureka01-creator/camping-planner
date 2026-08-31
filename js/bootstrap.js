f1050c0d1a263a0ef4fc4ccb79c80bd58c7869be
ureka01-creator <ureka01@nate.com>
2026-08-31 15:15:17 +0900

    Load home order customization

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -61,6 +61,7 @@ await import('./items-hub.js?v=067');
 await import('./item-edit-fix.js?v=082');
 await import('./edit-icons.js?v=081');
 await import('./admin-access.js?v=083');
+await import('./home-order.js?v=084');
 
 repairTripDatesIfNeeded().catch(error => {
   if (error?.code === 'ADMIN_REQUIRED') return;
